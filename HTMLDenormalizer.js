7bb3d7d3e96cab5b0a988bc75c7e323511740edb
Nkrumah <nkrumahthis@gmail.com>
2021-11-02 12:08:07 +0000

    initial commit

---
+++ HTMLDenormalizer.js
@@ -0,0 +1,137 @@
+function denormalize(inputed) {
+    // convert to javascript object
+    const inputObject = JSON.parse(inputed)
+
+    if (Array.isArray(inputObject)) {
+        response = ""
+        inputObject.forEach(inp => response += process(inp))
+        return response
+    }
+
+    return process(inputObject)
+
+}
+
+function process(inputObject) {
+    // use typeRouter to route to the right handler
+    const handler = typeRouter(inputObject.type)
+
+    // get element
+    const responseElement = handler(inputObject)
+
+    // return response
+    return responseElement
+}
+
+function typeRouter(objectType) {
+    // if type is text, use handleText
+    switch (objectType) {
+        case "Text": return handleText
+        case "Title": return handleTitle
+        case "Image": return handleImage
+        case "Section": return handleSection
+        case "Table": return handleTable
+        case "List": return handleList
+        default: return handleNotFound
+    }
+}
+
+function handleText(inputObject) {
+    // turn textObject into an <p> html element
+    return "<p>" + inputObject.content + "</p>"
+}
+
+function handleTitle(inputObject) {
+    // turn textObject into an <p> html element
+    return "<h1>" + inputObject.content + "</h1>"
+}
+
+function handleImage(inputObject) {
+    // turn textObject into an <p> html element
+    return '<img src="' + inputObject.content + '"/>'
+}
+
+function handleSection(inputObject) {
+    sectionResponse = `<div>`
+
+    inputObject.children.forEach(child => {
+        sectionResponse += process(child)
+    });
+
+    sectionResponse += `</div>`
+
+    return sectionResponse
+
+}
+
+function handleTable(inputObject) {
+    var tableResponse = `<table>`
+
+    inputObject.content.forEach(part => {
+        tableResponse += `<tr>`
+
+        part.forEach(row => {
+
+            if (row.type === "Header") {
+                tableResponse += `<th>`
+                tableResponse += row.content[0].children[0].content
+                tableResponse += `</th>`
+
+            } else if (row.type === "Data") {
+                tableResponse += `<td>`
+                tableResponse += process(row.content[0])
+                tableResponse += `</td>`
+
+            } else {
+                tableResponse = `<null/>`
+                return tableResponse
+            }
+        })
+
+        tableResponse += `</tr>`
+
+    })
+
+    tableResponse += `</table>`
+
+    return tableResponse
+}
+
+function handleList(inputObject) {
+    var listResponse = ""
+
+    if (inputObject.list_type === "Unordered") {
+        listResponse = "<ul>"
+        closingTag = "</ul>"
+    } else if (inputObject.list_type === "Ordered") {
+        listResponse = "<ol>"
+        closingTag = "</ol>"
+    } else {
+        return "<null/>"
+    }
+
+    inputObject.content.forEach(item => {
+        listResponse += "<li>"
+
+        if (item.content[0].children[0].type === "Text") {
+            listResponse += item.content[0].children[0].content
+        } else {
+            // item.content[0].children.forEach(child => listResponse += process(child))   
+            listResponse += process(item.content[0])
+        }
+        // listResponse += (item.content[0].children[0].type === "Text") ? item.content[0].children[0].content : item.content[0].children.forEach(child => process(child))
+        listResponse += "</li>"
+    })
+
+    listResponse += closingTag
+    return listResponse
+}
+
+
+function handleNotFound(content) {
+    return "<p>invalid object</p>"
+}
+
+
+
+module.exports.denormalize = denormalize;
